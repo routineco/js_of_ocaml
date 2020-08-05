81e3d719b18df116e077ac5a0da0db456b53527a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-05 17:50:57 +0000

    Deploy ocsigen/js_of_ocaml to github.com/ocsigen/js_of_ocaml.git:gh-pages

--- latest/manual/files/toplevel/test_dynlink.js
+++ latest/manual/files/toplevel/test_dynlink.js
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
 <html>
-    <head><meta http-equiv="refresh" content="0; URL=../../3.6.0/manual/files/toplevel/test_dynlink.js" /></head>
+    <head><meta http-equiv="refresh" content="0; URL=../../3.7.0/manual/files/toplevel/test_dynlink.js" /></head>
     <body></body>
 </html>

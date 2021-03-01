adcfea541262ae2d518c4bb02e2492c05a89e1e1
balat <balat@users.noreply.github.com>
2021-03-01 15:24:07 +0000

    Deploying to gh-pages from @ ocsigen/js_of_ocaml@bae96ec28fccc1058a9853089d0af7091c8577f5 🚀

--- 1.2/manual/files/planet/planet.js
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head><meta http-equiv="refresh" content="0; URL=../../1.4/manual/files/planet/planet.js" /></head>
-    <body></body>
-</html>
--- 1.3/manual/files/toplevel/toplevel.js
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head><meta http-equiv="refresh" content="0; URL=../../1.4/manual/files/toplevel/toplevel.js" /></head>
-    <body></body>
-</html>
